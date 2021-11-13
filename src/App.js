13384d404ec1b041559ad1868cf954498f829381
Rohald van Merode <rohald_89@live.nl>
2021-11-13 14:17:07 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 import './App.css';
+import apiKey from './config';
+import Gallery from './components/Gallery';
+import Nav from './components/Nav';
 import SearchForm from './components/SearchForm';
 
 function App() {
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const query = 'cars';
+
+    useEffect(() => {
+        const fetchImages = async () => {
+            setIsLoading(true);
+            const result = await axios(
+                `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&sort=relevance&format=json&nojsoncallback=1`
+            );
+            setImages(result.data.photos.photo);
+            setIsLoading(false);
+        };
+        fetchImages(); // eslint-disable-next-line
+    }, [query]);
+
     return (
-        <div className="container">
-            <SearchForm />
-        </div>
+        <BrowserRouter>
+            <div className="container">
+                <SearchForm />
+                <Nav />
+                <Gallery />
+                {isLoading ? (
+                    <div className="loading">Loading...</div>
+                ) : (
+                    <Routes>
+                        <Route
+                            exact
+                            path="/"
+                            element={<Navigate replace to="/cars" />}
+                        />
+                        <Route
+                            path="/cars"
+                            element={<Gallery images={images} />}
+                        />
+                        <Route
+                            path="/summer"
+                            element={<Gallery images={images} />}
+                        />
+                        <Route
+                            path="/winter"
+                            element={<Gallery images={images} />}
+                        />
+                    </Routes>
+                )}
+            </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
-    return <div></div>;
+    return (
+        <nav class="main-nav">
+            <ul>
+                <li>
+                    <Link to="/cars">Cars</Link>
+                </li>
+                <li>
+                    <Link to="/summer">Summer</Link>
+                </li>
+                <li>
+                    <Link to="/winter">Winter</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Nav;
